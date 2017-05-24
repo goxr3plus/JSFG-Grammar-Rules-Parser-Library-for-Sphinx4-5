bf6cf58e8978413d72089dffe8ff4ac6872b0bec
GOXR3PLUS <crAlexcomp@gmail.com>
2017-05-25 02:48:40 +0300

    Added the code

---
+++ README.md
@@ -0,0 +1,32 @@
+# A user in youtube asked me
+
+I have got a question. Is that possible to get known, which grammar rule was recognized? Some variable which returns "feelings" , "syntax" etc. ??
+
+For example we have the grammar:
+
+   #JSGF V1.0;
+
+  /**
+  * JSGF Grammar 
+  */
+
+  grammar grammar;
+
+  public <feelings>  = ( how are you | say hello);
+  public <voices>  = ( change to voice one  | change to voice two | change to voice three );
+  public <amazing>  = ( say amazing | what day is today );
+  public <nervous>  = (who is your daddy | obey monster | hey boss);
+  public <number> = ( zero | one | two | three | four | five | six | seven | nine | ten
+                   | eleven | twelve | thirteen | fourteen | fifteen | sixteen | seventeen | eighteen | nineteen | twenty 
+                   | thirty | forty | fifty | sixty | seventy | eighty | ninety 
+		           | hundred | thousand | million | billion)+;                   
+  public <syntax> = <number>{1} (plus | minus | multiply | division){1} <number>{1}; 
+
+#My answer
+
+Yes you can do it using the simple library i have written here , just one class :)
+
+#Warning
+
+I have to fix some things like for example if you search for the word `zero` it will return the rule `number` but not the rule `syntax` which obviously contains
+the word `zero` but it has it like `<number>{1}` , so i must add more code to detect this also .
---
+++ src/library/JSGFGrammarParser.java
@@ -0,0 +1,112 @@
+package library;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * The grammar file must be correctly formated in order for this class to work.
+ * 
+ * <br>
+ * Please see: <a href="https://www.w3.org/TR/jsgf/"> Link </a>
+ * 
+ * @author GOXR3PLUS
+ *         [[SuppressWarningsSpartan]]
+ */
+public class JSGFGrammarParser {
+	
+	/**
+	 * This class must not be initialised
+	 */
+	private JSGFGrammarParser() {
+	}
+	
+	/**
+	 * Opens the specified .gram file and finds the rules that contain the given
+	 * words , based on the below parameters :
+	 * 
+	 * <hr>
+	 * If for example we have [<b> public `feelings` = ( how are you | say
+	 * hello); </b>] . The rule is <b>`feelings`</b><br>
+	 * 
+	 * <br>
+	 * Big Problem is that ->it doesn't check if the rules contains other rules
+	 * that contain the
+	 * given
+	 * words , so this rule contains the given words , for example :
+	 * 
+	 * <pre>
+	 *  <code>
+	 *   public {@code <number> }= ( zero | one | two | three | four | five | six | seven | nine | ten
+	 *        | eleven | twelve | thirteen | fourteen | fifteen | sixteen | seventeen | eighteen | nineteen | twenty 
+	 *        | thirty | forty | fifty | sixty | seventy | eighty | ninety 
+	 *        | hundred | thousand | million | billion)+;                   
+	 *   public {@code <syntax> } = {@code <number>}{1} (plus | minus | multiply | division){1} {@code <number>}{1}; 
+	 *  </code>
+	 * </pre>
+	 * 
+	 * @param absoluteFilePath
+	 *        <br>
+	 *        The absolute path of the grammar file that you want to read
+	 * @param words
+	 *        <br>
+	 *        A list of words that you want to check and get the
+	 *        rules containing them
+	 * @param containAllWords
+	 *        <br>
+	 *        <b>True</b> if you want the rule to contain all the given words
+	 *        <br>
+	 *        <b>False</b> if you want the rule to contain any of the given
+	 *        words
+	 * @return ArrayList
+	 *         <br>
+	 *         of the rules that are containing [all or some ]
+	 *         of the words given based on the third parameter [
+	 *         ruleMustContainAllWords] of the method
+	 */
+	public static List<String> getRulesContainingWords(String absoluteFilePath , List<String> words , boolean containAllWords) {
+		
+		//Store the rules containing this word inside an ArrayList		
+		ArrayList<String> rules = new ArrayList<>();
+		
+		//Read file into stream, try-with-resources
+		try (Stream<String> stream = Files.lines(Paths.get(absoluteFilePath))) {
+			
+			//Collect all the lines to one
+			String oneLine = stream.map(String::trim).collect(Collectors.joining());
+			
+			//-------------Print the line - For testing----------------------
+			//System.out.println(oneLine);
+			
+			//Split by ; and go
+			Arrays.asList(oneLine.split(";")).stream().forEach(line -> {
+				
+				//Check if line is a rule 
+				if (!line.contains("<") || !line.contains(">") || !line.contains("="))
+					return;
+				
+				//System.out.println("Line is a rule->" + line + "\n"); //TESTING CODE IGNORE IT
+				
+				//-------------------If the rule must [contain all] the given words
+				if (containAllWords) {
+					if (words.stream().allMatch(line.split("=")[1]::contains))
+						rules.add(line.split("\\>")[0].split("\\<")[1]); //Get the rule		
+						
+					//---------------If the rule contains can [contain any] of the given words
+				} else if (words.stream().anyMatch(line.split("=")[1]::contains))
+					
+					rules.add(line.split("\\>")[0].split("\\<")[1]); //Get the rule		
+			});
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return rules;
+	}
+}
